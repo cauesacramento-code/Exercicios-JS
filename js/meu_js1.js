818c1aaf54e357112432c464fea560ce0917d3f2
cauesacramento-code <caue.sacramento@edu.senai.br>
2026-06-16 09:56:06 -0300

    c

---
+++ js/meu_js1.js
@@ -0,0 +1,14 @@
+     var texto = "teste de texto";
+        var Numberinteiro = 42;
+        var numberfracionado = 3.14;
+        var teste = true;
+        var nome = "João";
+        var idade = 30;
+        var salario = 2500;
+        alert(texto); 
+    document.write(texto);
+    console.log(texto);
+  
+var nome = prompt('Qual seu nome:')
+var idade = prompt('Qual sua idade:')
+document.write('<h1>Olá ' + nome +', tudo bem? sua  idade é ' + idade + ' anos, correto?</h1>');
